a8deb345da3e531a4d6956169af00e275170b7b8
haadibolotbekov <ebashit.na@gmail.com>
2023-12-31 13:25:35 +0300

    Add first commit

---
+++ src/main/java/com/neobis/neo_quiz/NeoQuizApplication.java
@@ -0,0 +1,13 @@
+package com.neobis.neo_quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoQuizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neobis/neo_quiz/NeoQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neobis.neo_quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
