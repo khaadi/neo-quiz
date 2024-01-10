92e54c1b557f08766b7c020b2869c30fbb94b23f
khaadi <63555893+khaadi@users.noreply.github.com>
2024-01-10 23:22:23 +0300

    Update ArticleController.java

--- src/main/java/com/neobis/neo_quiz/controller/ArticleController.java
+++ src/main/java/com/neobis/neo_quiz/controller/ArticleController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
